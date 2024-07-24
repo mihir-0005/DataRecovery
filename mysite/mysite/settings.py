01db809eec1d5704e6bd7f1c138355c27f22de3f
mihir-0005 <110338461+mihir-0005@users.noreply.github.com>
2024-07-25 01:20:56 +0530

    This commit is a mistake

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,9 +26,6 @@ SECRET_KEY = 'django-insecure-0r901v6#txw-*2530+opt$*hl!weny_d2q-g_v-vg)bga$f*ef
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
@@ -86,9 +82,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost:5432/DataRecovery')
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
